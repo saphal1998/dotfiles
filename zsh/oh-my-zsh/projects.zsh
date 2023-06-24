145b0a10e6217f133a1053391c2be90adc7671d6
Saphal Patro <saphal1998@gmail.com>
2023-06-23 20:23:18 -0400

    Added project automagically from Developer directory

--- zsh/oh-my-zsh/projects.zsh
+++ zsh/oh-my-zsh/projects.zsh
@@ -1,1 +1,11 @@
+#/usr/bin/env bash
+
 # We can import project directories here, and use cd $project_name to directly go to that folder
+DIRECTORIES_PATH="$HOME/Developer"
+
+for PROJECT_PATH in "$DIRECTORIES_PATH"/*; do
+    if [ -d "$PROJECT_PATH" ]; then
+        PROJECT_NAME=$(basename "$PROJECT_PATH")
+        alias "\$$PROJECT_NAME"="cd \"$PROJECT_PATH\""
+    fi
+done
