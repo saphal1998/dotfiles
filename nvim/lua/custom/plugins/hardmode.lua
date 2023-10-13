3d58ff9750229ab6844868a69cefb7ab9ea40271
Saphal Patro <saphal1998@gmail.com>
2023-10-13 12:00:33 -0400

    Moved to lazyvim to see what its like

--- nvim/lua/custom/plugins/hardmode.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"m4xshen/hardtime.nvim",
-	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-	opts = {},
-	config = function()
-		local hardtime = require("hardtime")
-		hardtime.setup()
-	end
-}
