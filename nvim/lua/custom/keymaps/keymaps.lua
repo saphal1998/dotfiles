fdb87cd976d3313abb6574570d85a40f7d30cd9e
Saphal Patro <saphal1998@gmail.com>
2023-10-13 13:48:44 -0400

    Added undotree and diffview

--- nvim/lua/custom/keymaps/keymaps.lua
+++ nvim/lua/custom/keymaps/keymaps.lua
@@ -1,4 +1,10 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set('t', '<C-x>', "<C-\\><C-n><C-w>h", { silent = true })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving down half-page" })
+-- Netrw
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('t', '<C-x>', '<C-\\><C-n><C-w>h', { silent = true })
+
+-- Moving around
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Center cursor after moving down half-page' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Center cursor after moving down half-page' })
+
+-- Undotree
+vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle, { desc = '[u]ndotree[t]oggle' })
---
+++ nvim/lua/custom/plugins/hardmode.lua
@@ -0,0 +1,9 @@
+return {
+  'm4xshen/hardtime.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+  opts = {},
+  config = function()
+    local hardtime = require 'hardtime'
+    hardtime.setup()
+  end,
+}
