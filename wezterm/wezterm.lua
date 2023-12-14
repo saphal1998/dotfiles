453b289372e2e82daca1ec2df665fa78f7de4367
Saphal Patro <saphal1998@gmail.com>
2023-12-13 21:41:25 -0500

    Added wezterm modifications

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require "wezterm"
 
-function scheme_for_appearance(appearance)
+local function scheme_for_appearance(appearance)
 	if appearance:find "Dark" then
 		return "Catppuccin Mocha"
 	else
@@ -10,4 +10,18 @@ end
 
 return {
 	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+	use_fancy_tab_bar = false,
+	enable_scroll_bar = false,
+	window_padding = {
+		top = 0,
+		bottom = 0,
+		left = 0,
+		right = 0
+	},
+	tab_bar_at_bottom = true,
+
+	font = wezterm.font_with_fallback {
+		'FiraCode Nerd Font',
+	},
+	font_size = 16
 }
