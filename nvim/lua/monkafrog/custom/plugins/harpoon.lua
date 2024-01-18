9bd60cd4cb1da61db9b8a6900dc14391fb91f219
Saphal Patro <saphal1998@gmail.com>
2024-01-18 22:42:48 +0530

    Modified harpoon keymaps

--- nvim/lua/monkafrog/custom/plugins/harpoon.lua
+++ nvim/lua/monkafrog/custom/plugins/harpoon.lua
@@ -8,23 +8,23 @@ return {
     telescope.load_extension('harpoon')
     harpoon:setup()
 
-    vim.keymap.set('n', '<C-h>m', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+    vim.keymap.set('n', 'hm', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
       { desc = '[h]arpoon [m]enu' })
 
-    vim.keymap.set('n', '<leader>fl', function()
+    vim.keymap.set('n', 'hi', function()
       harpoon:list():append()
     end, { desc = '[h]arpoon add [f]i[l]e' })
 
-    vim.keymap.set('n', '<C-q>', function()
+    vim.keymap.set('n', 'hq', function()
       harpoon:list():select(1)
     end, { desc = '[h]arpoon jump to file [' .. 1 .. ']' })
-    vim.keymap.set('n', '<C-w>', function()
+    vim.keymap.set('n', 'hw', function()
       harpoon:list():select(2)
     end, { desc = '[h]arpoon jump to file [' .. 2 .. ']' })
-    vim.keymap.set('n', '<C-e>', function()
+    vim.keymap.set('n', 'he', function()
       harpoon:list():select(3)
     end, { desc = '[h]arpoon jump to file [' .. 3 .. ']' })
-    vim.keymap.set('n', '<C-r>', function()
+    vim.keymap.set('n', 'hr', function()
       harpoon:list():select(4)
     end, { desc = '[h]arpoon jump to file [' .. 4 .. ']' })
   end,
