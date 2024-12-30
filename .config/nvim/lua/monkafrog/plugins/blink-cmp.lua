755258150ed385599a80b49ba3f09685724b6dc3
monkafrog <saphal1998@gmail.com>
2024-12-29 23:19:26 -0500

    changed settings

--- .config/nvim/lua/monkafrog/plugins/blink-cmp.lua
+++ .config/nvim/lua/monkafrog/plugins/blink-cmp.lua
@@ -6,7 +6,6 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    keymap = { preset = 'default' },
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono',
